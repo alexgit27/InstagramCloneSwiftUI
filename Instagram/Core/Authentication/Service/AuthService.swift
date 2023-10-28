5cd058c4aa8f41cf6cc9b2e77661ff99c6f23e9e
alexgit27 <20alexandr21@gmail.com>
2023-10-28 18:58:44 +0300

    Add Upload User Data to Firestore

--- Instagram/Core/Authentication/Service/AuthService.swift
+++ Instagram/Core/Authentication/Service/AuthService.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 
 class AuthService {
     @Published var userSession: FirebaseAuth.User?
@@ -32,6 +34,7 @@ class AuthService {
         do {
             let result = try await Auth.auth().createUser(withEmail: email, password: password)
             self.userSession = result.user
+            await uploadUserData(uid: result.user.uid, username: username, email: email)
         } catch {
             print("DEBUG: Error occur in createUser: \(error.localizedDescription)")
         }
@@ -44,5 +47,11 @@ class AuthService {
     func signout() {
         try? Auth.auth().signOut()
         self.userSession = nil
+    }
+    
+    private func uploadUserData(uid: String, username: String, email: String) async {
+        let user = User(id: uid, username: username, email: email)
+        guard let encodedUser = try? Firestore.Encoder().encode(user) else { return }
+        try? await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
     }
 }
