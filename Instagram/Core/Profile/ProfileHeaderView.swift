36fb44f897d997eb2b67d8199befd15dbab0a174
alexgit27 <20alexandr21@gmail.com>
2023-10-30 21:32:36 +0200

    Add Flow for Edit Profile / Follow Button

--- Instagram/Core/Profile/ProfileHeaderView.swift
+++ Instagram/Core/Profile/ProfileHeaderView.swift
@@ -44,16 +44,22 @@ struct ProfileHeaderView: View {
             .padding(.horizontal)
             
             Button {
-                 
+                if user.isCurrentUser {
+                    print("Edit Profile...")
+                } else {
+                    print("Follow User...")
+                }
             } label: {
-                Text("Edit Profile")
+                Text(user.isCurrentUser ? "Edit Profile" : "Follow")
                     .font(.subheadline)
                     .fontWeight(.semibold)
                     .frame(width: 360, height: 32)
-                    .foregroundColor(.black)
+                    .background(user.isCurrentUser ? .white : Color(.systemBlue))
+                    .foregroundColor(user.isCurrentUser ? .black : .white)
+                    .cornerRadius(6)
                     .overlay(
                     RoundedRectangle(cornerRadius: 6)
-                        .stroke(Color.gray, lineWidth: 1.0)
+                        .stroke(user.isCurrentUser ? .gray : .clear, lineWidth: 1.0)
                     )
             }
             
